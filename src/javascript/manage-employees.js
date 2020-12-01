ca7bd960dbac8c1bba550c69b72c2edb90b88d41
HootDunk <58009556+HootDunk@users.noreply.github.com>
2020-11-30 18:34:58 -0600

    styled the add drivers card and fixed radio button

--- src/javascript/manage-employees.js
+++ src/javascript/manage-employees.js
@@ -8,7 +8,6 @@ const modalDriverForm = document.querySelector("#editDriverForm");
 const updateDriverBtn = document.querySelector("#modalUpdateDriver");
 // const deleteDriverBtn = document.querySelector("#modalDeleteDriver");
 const addUserCard = document.querySelector("#add-user-card");
-// 
 const newDriverForm = document.querySelector("#newDriverForm");
 
 // set modal values to corresponding driver
@@ -16,22 +15,26 @@ const setDriverFormValues = (doc) => {
     // give buttons a data-id that matches the job
     let rateLabel = document.getElementById("rate-label");
     updateDriverBtn.dataset.id = doc.id;
+    // console.log(modalDriverForm['driverTypeRadio'][0])
+    // console.log(modalDriverForm['driverTypeRadio'][1])
 
     if (doc.data().type == "salary"){
       modalDriverForm['salaryRadio'].checked = true;
       rateLabel.innerHTML = "Pay Per Mile: ";
-      modalDriverForm['rate'].value = doc.data().rate;
-      modalDriverForm['address'].value = doc.data().address;
-      modalDriverForm['email'].value = doc.data().email;
-
     }
     else if (doc.data().type == "owner-operator"){
       modalDriverForm['ownerOpRadio'].checked = true;
       rateLabel.innerHTML = "Load Rate: ";
-      modalDriverForm['rate'].value = doc.data().rate;
-      modalDriverForm['address'].value = doc.data().address;
-      modalDriverForm['email'].value = doc.data().email;
     }
+    if (doc.data().status == "active"){
+      modalDriverForm["statusRadio1"].checked = true;
+    }
+    else if (doc.data().status == "inactive"){
+      modalDriverForm["statusRadio2"].checked = true;
+    }
+    modalDriverForm['rate'].value = doc.data().rate;
+    modalDriverForm['address'].value = doc.data().address;
+    modalDriverForm['email'].value = doc.data().email;
     $('#editDriverModal').modal()
 }
 
@@ -49,12 +52,14 @@ const displayDrivers = ((data) => {
     })
 
     drivers.sort((a, b) => a.data().lname.localeCompare(b.data().lname))
+    drivers.sort((a, b) => a.data().status.localeCompare(b.data().status))
     let html = "";
 
     drivers.forEach((driver, index) => {
+      
       if(index == 0){
         const card = `
-        <div id="add-driver-card" class="card bg-secondary mx-2 mb-3 input-card" style="width: 15rem;">
+        <div id="add-driver-card" class="card bg-primary mx-2 mb-3 input-card" style="width: 15rem;">
         <div class="card-header text-white bg-dark h5">
         Add New Drivers
         </div>
@@ -62,33 +67,48 @@ const displayDrivers = ((data) => {
           <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
         </svg>
         <div class="card-body">
-          <h4 class="card-text text-center">Click here to add new drivers to the system</h4>
+          <h4 class="card-text text-white text-center">Click here to add new drivers</h4>
         </div>
       </div>
         `;
         html += card;
       }
-      else {
-        // could break up this text to add the conditional and add the html after instead of having two whole separate ones
+
+      let title = "";
+      if (driver.data().type == "salary"){
+        title = "Pay Rate Per Mile";
+        console.log(title)
+      }
+      else if (driver.data().type == "owner-operator"){
+        title = "Pay Rate Per Load";
+      }
+
+      let background = "";
+      if (driver.data().status == "active"){
+        background = "bg-dark";
+      }
+      else if (driver.data().status == "inactive"){
+        background = "bg-secondary";
+      }
+
+
+        // break up this text to add the conditional and add the html after instead of having two whole separate ones
         const card = `
         <div class="card card-custom mx-2 mb-3" style="width: 15rem;">
-        <div class="card-header text-white bg-dark h5">
+        <div class="card-header text-white ${background} h5 ">
           ${driver.data().fname} ${driver.data().lname}
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">Type: ${driver.data().type}</li>
-          <li class="list-group-item">Status: ${driver.data().status}</li>
-          <li class="list-group-item">Pay Rate: ${driver.data().rate}</li>
-          <li class="list-group-item overflow-auto">Address:</br> ${driver.data().address}</li>
-          <li class="list-group-item overflow-auto">Email:</br> ${driver.data().email}</li>
+          <li class="list-group-item"><b>Type</b>: ${driver.data().type}</li>
+          <li class="list-group-item"><b>Status</b>: ${driver.data().status}</li>
+          <li class="list-group-item"><b>${title}</b>: ${driver.data().rate}</li>
+          <li class="list-group-item overflow-auto"><b>Address</b>:</br> ${driver.data().address}</li>
+          <li class="list-group-item overflow-auto"><b>Email</b>:</br> ${driver.data().email}</li>
         </ul>
-        <button data-id="${driver.id}" type="button" class="btn btn-light">Edit</button>
+        <button data-id="${driver.id}" type="button" class="btn btn-light"><b>Edit</b></button>
         </div>
         `;
         html += card;
-      }
-    
-      
     })
     driverCardDiv.innerHTML = html;
 
@@ -96,7 +116,8 @@ const displayDrivers = ((data) => {
     document.querySelectorAll(".btn.btn-light").forEach(btn => {
       btn.addEventListener("click", event => {
         // call modal
-        const driverID = event.target.dataset.id;
+        const driverID = event.currentTarget.dataset.id;
+        console.log(driverID)
         db.read.getDriverDoc(setDriverFormValues, driverID)
       })
     })
@@ -126,10 +147,10 @@ const displayUsers = ((data) => {
         ${user.data().fname} ${user.data().lname}
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">Role: ${user.data().role}</li>
-        <li class="list-group-item overflow-auto">Email:<br> ${user.data().email}</li>
+        <li class="list-group-item"><b>Role</b>: ${user.data().role}</li>
+        <li class="list-group-item overflow-auto"><b>Email</b>:<br> ${user.data().email}</li>
       </ul>
-      <button data-id="${user.id}" type="button" class="btn btn-light">Edit</button>
+      <button data-id="${user.id}" type="button" class="btn btn-light"><b>Edit</b></button>
       </div>
       `;
       html += card;
